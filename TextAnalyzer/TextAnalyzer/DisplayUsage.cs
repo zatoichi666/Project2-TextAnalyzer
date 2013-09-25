48b3c4f715b867ca93f98a24cb590609ac47f0ae
zatoichi666 <matthewsynborski@gmail.com>
2013-09-25 14:47:46 -0400

    Initial Commit

---
+++ TextAnalyzer/TextAnalyzer/DisplayUsage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyzer
+{
+    public static class DisplayUsage
+    {
+
+        public static void toConsole()
+        {
+            Console.WriteLine("TextAnalyzer by Matthew Synborski");
+            Console.WriteLine("Usage: TextAnalyzer [OPTIONS]");
+            Console.WriteLine("   or: TextAnalyzer path [OPTIONS]");
+            Console.WriteLine("");
+            Console.WriteLine("/R,           Recursively search");
+            Console.WriteLine("/T,           Text query");
+            Console.WriteLine("    /O,       -must contain one of text strings: default");
+            Console.WriteLine("    /A,       -must contain all text strings");            
+            Console.WriteLine("/M,           Metadata query");
+            Console.WriteLine("/C,           Category query");
+            Console.WriteLine("/X,           File Extension (one /X for each extension)");
+                      
+
+        }
+    }
+}
---
+++ TextAnalyzer/TextAnalyzer/Executive.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyzer
+{
+    class Executive
+    {
+        static void Main(string[] args)
+        {
+            CommandLineProcessor cmd = new CommandLineProcessor(args);
+            Job _job = cmd.getJob();
+
+            if (_job.resultStringList.Count > 0)
+            {
+                Console.WriteLine("There were errors:");
+                foreach (String errString in _job.resultStringList)
+                {
+                    Console.WriteLine(errString);
+                }
+                Console.WriteLine("");
+                DisplayUsage.toConsole();
+            }
+            else
+            {
+                Console.Write("Searching for ");
+                foreach (String s in _job.textQueryList)
+                {
+                    Console.Write(s);
+                    Console.Write(", ");
+                }
+                Console.WriteLine();
+                IEnumerable<string> fileTable = FileTable.GetFiles(_job.taskPath, _job.extensionList.ToArray(), _job.recursive);
+
+                if (_job.queryType == "text")
+                {
+                    Console.WriteLine("Printing the results of the text query");
+                    List<String> tq = TextQuery.run(fileTable, _job.textQueryList.ToArray(), _job.mustFindAll);
+                    foreach (String s in tq)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+        }
+    }
+}
